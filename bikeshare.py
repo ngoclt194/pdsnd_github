101752a4e3927a60601220bf99266f37e9e82753
NgocLuu <ngoclt194@gmail.com>
2023-07-08 15:49:07 +0700

    comment: change comment

--- bikeshare.py
+++ bikeshare.py
@@ -9,7 +9,7 @@ CITY_DATA = {'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks document user to specify a city, month, and day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
