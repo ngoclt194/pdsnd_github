898c1edd5f4713771b4d64e0b2e3c84b6a2a7094
Luu Ngoc <58684354+ngoclt194@users.noreply.github.com>
2023-07-08 16:16:33 +0700

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -9,7 +9,7 @@ CITY_DATA = {'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks document user to specify a city, month, and day to analyze.
+    Asks  user to specify a city, month, and day to analyze.
 
     Returns:
         (str) city - name of the city to analyze
